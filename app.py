4ba8caad671ebfdf884f18b0e5aff3ce3d3be3b1
StudioMitesh <miteshmanishshah@gmail.com>
2023-11-29 10:15:17 -0500

    update the html website and finish first flask

--- app.py
+++ app.py
@@ -1,78 +1,29 @@
 from flask import Flask, render_template, request, redirect, url_for
 import mysql.connector
-import sqlite3
-from flaskext.mysql import MySQL
+import pandas as pd
 
 app = Flask(__name__)
 
-# Configuration
-DATABASE = 'database.db'
-mysql0 = MySQL()
-app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
-app.config['MYSQL_DATABASE_DB'] = 'EmpData'
-app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.config["DEBUG"] = True
 
-mysql0.init_app(app=app)
+db = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="password",
+    database='tsa-sql-database'
+)
 
-from flask import Flask
-import MySQLdb
-
-app = Flask(__name__)
-
-db = MySQLdb.connect("localhost", "root", "yourDbPassWord", "DBname")
-
-@app.route('/api/v1.0/items', methods=['GET'])
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True)
-    email = db.Column(db.String(120), unique=True)
-
-    def __init__(self, username, email):
-        self.username = username
-        self.email = email
-
-    def __repr__(self):
-        return '<User %r>' % self.username
-
-
-def create_table():
-    with sqlite3.connect(DATABASE) as conn:
-        cursor = conn.cursor()
-        cursor.execute('''
-            CREATE TABLE IF NOT EXISTS tasks (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                title TEXT NOT NULL
-            )
-        ''')
-        conn.commit()
+connection = mysql.connector.connect(**db)
+cursor = connection.cursor()
 
 @app.route('/')
 def index():
-    create_table()
-    with sqlite3.connect(DATABASE) as conn:
-        cursor = conn.cursor()
-        cursor.execute('SELECT * FROM tasks')
-        tasks = cursor.fetchall()
-    return render_template('index.html', tasks=tasks)
+    # Example query
+    query = "SELECT * FROM member;"
+    cursor.execute(query)
+    data = cursor.fetchall()
 
-@app.route('/add', methods=['POST'])
-def add():
-    title = request.form['title']
-    with sqlite3.connect(DATABASE) as conn:
-        cursor = conn.cursor()
-        cursor.execute('INSERT INTO tasks (title) VALUES (?)', (title,))
-        conn.commit()
-    return redirect(url_for('index'))
-
-@app.route('/delete/<int:task_id>')
-def delete(task_id):
-    with sqlite3.connect(DATABASE) as conn:
-        cursor = conn.cursor()
-        cursor.execute('DELETE FROM tasks WHERE id = ?', (task_id,))
-        conn.commit()
-    return redirect(url_for('index'))
+    return render_template('index.html', data=data)
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('searchBtn').addEventListener('click', function() {
+    var input = document.getElementById('searchInput').value;
+    alert('Search for ' + input);
+    // Here you can add the logic to search through the tables
+});
